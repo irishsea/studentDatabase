4fcf0bba0444758d1b6f62bdc0c586b58fe28cb1
irishsea <iriserggeeva@gmail.com>
2022-03-29 17:36:39 +0300

    added tests for services

---
+++ src/test/java/services/StudentServiceTest.java
@@ -0,0 +1,40 @@
+package services;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+        GroupService groupService = new GroupService();
+        StudentService studentService = new StudentService();
+
+
+    }
+
+    @Test
+    void findStudentTest() {
+    }
+
+    @Test
+    void saveStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+
+    @Test
+    void filterByFirstName() {
+    }
+
+    @Test
+    void filterByGroup() {
+    }
+}
